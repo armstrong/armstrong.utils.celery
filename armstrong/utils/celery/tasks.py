c29c2113656698498d2e573a647a876b1546c296
Travis Swicegood <development@domain51.com>
2011-10-28 11:23:29 -0500

    update this to remove the reliance on instance

--- armstrong/utils/celery/tasks.py
+++ armstrong/utils/celery/tasks.py
@@ -10,17 +10,18 @@ class SignalTask(Task):
     abstract = True
 
     def execute(self, request, pool, loglevel, logfile, **kwargs):
-        sender = tuple_to_model(request.kwargs["sender_tuple"])
-        instance_model = tuple_to_model(request.kwargs["instance_tuple"][:2])
-        try:
-            instance = instance_model.objects.get(pk=request.kwargs["instance_tuple"][-1])
-        except instance_model.DoesNotExist:
-            # TODO: retry
-            return
-        del request.kwargs["sender_tuple"]
-        del request.kwargs["instance_tuple"]
+        sender_tuple, instance_tuple = (request.kwargs.pop("sender_tuple"),
+                request.kwargs.pop("instance_tuple", None))
+        sender = tuple_to_model(sender_tuple)
         request.kwargs["sender"] = sender
-        request.kwargs["instance"] = instance
+        if instance_tuple is not None:
+            instance_model = tuple_to_model(instance_tuple[:2])
+            try:
+                instance = instance_model.objects.get(pk=instance_tuple[-1])
+                request.kwargs["instance"] = instance
+            except instance_model.DoesNotExist:
+                # TODO: retry?
+                pass
         super(SignalTask, self).execute(request, pool, loglevel, logfile, **kwargs)
 
     @classmethod
@@ -29,17 +30,15 @@ class SignalTask(Task):
         #       This has been tested manually, but there's no clean way to
         #       test it in an automated fashion.  Rather than delay the release,
         #       I'm getting it out there and we'll figure out the testing later.
-        sender, instance = kwargs.pop("sender"), kwargs.pop("instance")
-        sender_tuple = model_to_tuple(sender)
-        instance_tuple = model_to_tuple(instance) + (instance.pk, )
-
         # We can't send the signal because we can't serialize the thread lock
         if "signal" in kwargs:
             del kwargs["signal"]
-        kwargs.update({
-            "sender_tuple": sender_tuple,
-            "instance_tuple": instance_tuple,
-        })
+
+        sender, instance = kwargs.pop("sender"), kwargs.pop("instance", None)
+        kwargs["sender_tuple"] = model_to_tuple(sender)
+        if instance is not None:
+            kwargs["instance_tuple"] = model_to_tuple(instance) + (instance.pk, )
+
         return super(SignalTask, self).apply_async([], kwargs)
 
 
