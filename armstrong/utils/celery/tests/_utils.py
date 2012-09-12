587b253ca895251bd22ed68cf3a6b48d8add5f64
Travis Swicegood <development@domain51.com>
2012-09-12 12:02:52 -0500

    Convert to spaces (oops)

--- armstrong/utils/celery/tests/_utils.py
+++ armstrong/utils/celery/tests/_utils.py
@@ -8,14 +8,14 @@ class SimpleModel(models.Model):
 
 
 class TestCase(ArmstrongTestCase):
-	def setUp(self):
-		fudge.clear_calls()
-		fudge.clear_expectations()
-		super(TestCase, self).setUp()
+    def setUp(self):
+        fudge.clear_calls()
+        fudge.clear_expectations()
+        super(TestCase, self).setUp()
 
-	def tearDown(self):
-		fudge.verify()
-		super(TestCase, self).tearDown()
+    def tearDown(self):
+        fudge.verify()
+        super(TestCase, self).tearDown()
 
-	def assert_has_attr(self, obj, attr):
-		return self.assertTrue(hasattr(obj, attr))
+    def assert_has_attr(self, obj, attr):
+        return self.assertTrue(hasattr(obj, attr))
